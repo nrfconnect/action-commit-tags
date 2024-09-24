3634ac2b01c250072cfa272aa5b371664df89e0e
Carles Cufi <carles.cufi@nordicsemi.no>
2024-09-24 13:00:54 +0200

    action: Overhaul the action
    Remove the use of gitlint, instead just iterating over the commits
    manually.
    
    Use the PR SHAs to calculate the list of commits to check using the
    following reference:
    
    https://github.com/orgs/community/discussions/59677#discussioncomment-10782359
    
    Clean up the action's inputs to simplify it.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- action.py
+++ action.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import argparse
 import json
 import os
+import re
 import shlex
 import shutil
 import subprocess
@@ -37,8 +38,6 @@ PARSER.add_argument('-t', '--target', type=Path, required=True,
                     help='Target local repo path to run the check on')
 PARSER.add_argument('-b', '--baserev', default=None, required=True,
                     help='Base revision to use, all the way to HEAD')
-PARSER.add_argument('-r', '--revrange', default=None, required=True,
-                    help='Revision range to use in gitlint format')
 PARSER.add_argument('--pr', default=None, required=True,
                     help='<org>/<repo>/<pr num>')
 PARSER.add_argument('--upstream', default=None, required=True,
@@ -69,13 +68,20 @@ def parse_args():
 
     ARGS = PARSER.parse_args()
     
-    stdout(f'target: {ARGS.target} baserev: {ARGS.baserev} revrange: {ARGS.revrange}')
+    stdout(f'target: {ARGS.target} baserev: {ARGS.baserev}')
     stdout(f'pr: {ARGS.pr} upstream: {ARGS.upstream}')
 
-    if ARGS.baserev == 'none' and ARGS.revrange == 'none':
-        sys.exit('Must specify either baserev or revrange')
-    elif ARGS.baserev != 'none' and ARGS.revrange != 'none':
-        sys.exit('Must specify only one of baserev or revrange')
+    if ARGS.target == 'none':
+        sys.exit('--target is required')
+
+    if ARGS.upstream == 'none':
+        sys.exit('--upstream is required')
+
+    if ARGS.baserev == 'none' and ARGS.pr == 'none':
+            sys.exit('--baserev or --pr is required')
+
+    if ARGS.baserev != 'none' and ARGS.pr != 'none':
+            sys.exit('--baserev and --pr are mutually exclusive')
 
 def ssplit(cmd):
     if isinstance(cmd, str):
@@ -83,7 +89,7 @@ def ssplit(cmd):
 
     return cmd
 
-def runc(cmd, **kwargs):
+def runc(cmd, exit_on_cpe=True, **kwargs):
     # A shorthand for running a simple shell command.
 
     cwd = os.fspath(kwargs.get('cwd', os.getcwd()))
@@ -95,9 +101,17 @@ def runc(cmd, **kwargs):
         stdout(f'running "{cmd}" in "{cwd}"')
 
     kwargs['check'] = True
-    return subprocess.run(ssplit(cmd), **kwargs)
+    try:
+        ret = subprocess.run(ssplit(cmd), **kwargs)
+    except subprocess.CalledProcessError as e:
+        if exit_on_cpe:
+            sys.exit(f'Execution of {cmd} failed with {e.returncode}')
+        else:
+            raise
 
-def runc_out(cmd, **kwargs):
+    return ret
+
+def runc_out(cmd, exit_on_cpe=True, **kwargs):
     # A shorthand for running a simple shell command and getting its output.
 
     cwd = kwargs.get('cwd', os.getcwd())
@@ -110,8 +124,85 @@ def runc_out(cmd, **kwargs):
     kwargs['check'] = True
     kwargs['universal_newlines'] = True
     kwargs['stdout'] = subprocess.PIPE
-    cp = subprocess.run(ssplit(cmd), **kwargs)
-    return cp.stdout
+
+    try:
+        cp = subprocess.run(ssplit(cmd), **kwargs)
+    except subprocess.CalledProcessError as e:
+        if exit_on_cpe:
+            sys.exit(f'Execution of {cmd} failed with {e.returncode}')
+        else:
+            raise
+
+    return cp.stdout.rstrip()
+
+def fetch_upstream(gh, upstream):
+    pass
+
+def fetch_pr(gh, pr):
+    pass
+
+def merge_base(target, base, head):
+    mb = runc_out(f'git -C {target} merge-base {base} {head}')
+    stdout(f'merge base {mb}')
+    return mb
+
+def check_commit(urepo, target, sha):
+    stdout(f'Checking commit {sha}')
+    cm = runc_out(f'git -C {target} show -s --format=%B {sha}').split('\n')
+    title = cm[0]
+    body = '\n'.join(cm[1:])
+#    stdout(f'{title}')
+#    stdout(f'{body}')
+    m = re.match(r'^(Revert\s+\")?\[nrf (mergeup|fromtree|fromlist|noup)\]\s+',
+                 title)
+    if not m:
+        sys_exit(f'{sha}: Title does not contain a sauce tag')
+    revert = m.group(1)
+    tag = m.group(2)
+    if not tag:
+        sys_exit(f'{sha}: Title does not contain a sauce tag')
+    
+    #stdout(tag)
+
+    # Skip the rest of checks if the commit is a revert
+    if revert:
+        if tag == 'mergeup':
+            sys.exit('Mergeup cannot be reverted')
+        stdout(f'Revert commit, skipping additional checks')
+        return
+
+    if tag == 'mergeup':
+        # Count the merges in this commit range (sha^! is a range for sha
+        # itself)
+        count = runc_out('git rev-list --merges --count {sha}^!')
+        if count != '1':
+            sys.exit('mergeup used in a non-merge commit')
+        if not re.match(r'^\[nrf mergeup\] Merge upstream up to commit \b([a-f0-9]{40})\b',
+                         title):
+            sys.exit('{sha}: Invalid mergeup commit title')
+    elif tag == 'fromlist':
+        regex = r'^Upstream PR: (' \
+                r'https://github\.com/.*/pull/(\d+)|' \
+                r'http://lists.infradead.org/pipermail/hostap/.*\.html' \
+                r')'
+
+        match = re.search(regex, body, re.MULTILINE)
+        if not match:
+            sys_exit(f'{sha}: fromlist commit missing an Upstream PR reference')
+        
+        #stdout(f'fromlist: {match.group(1)}')
+        if urepo:
+            upr = match.group(2)
+            stdout(f'fromlist: {upr}')
+    elif tag == 'fromtree':
+        regex = r'^\(cherry picked from commit \b([a-f0-9]{40})\b\)'
+        match = re.search(regex, body, re.MULTILINE)
+        if not match:
+            sys_exit(f'{sha}: fromtree commit missing cherry-pick reference')
+        #stdout(f'fromtree: {match.group(0)}')
+        if urepo:
+            usha = match.group(1)
+            stdout(f'fromtree: {usha}')
 
 def main():
     parse_args()
@@ -121,28 +212,43 @@ def main():
 
     gh = Github(token or None)
 
-    org_str, repo_str, pr_num = gh_pr_split(ARGS.pr)
+    dcommits = []
+    dshas = []
 
     target = Path(ARGS.target).absolute()
-
     if not target.is_dir():
         sys.exit(f'target repo {target} does not exist; check path')
 
-    if ARGS.revrange == 'none':
-        try:
-            revs = runc_out(f'git -C {target} rev-list --first-parent {ARGS.baserev}..HEAD')
-            revs = revs.replace('\n', ',')
-        except subprocess.CalledProcessError:
-            sys.exit('git execution exited with error')
-    else:
-        revs = ARGS.revrange
+    org_str, repo_str, br_str = gh_pr_split(ARGS.upstream)
+    urepo = gh.get_repo(f'{org_str}/{repo_str}')
 
-    stdout(f'Running {PROG} checks on target {target} for range {revs}')
-    try:
-        runc(f'gitlint --target {target} -c ncs-sauce-tags.enable=true ' \
-             f'--commits {revs}', cwd=os.path.dirname(__file__))
-    except subprocess.CalledProcessError:
-        sys.exit('gitlint execution exited with error')
+    if ARGS.pr != 'none':
+        org_str, repo_str, pr_str = gh_pr_split(ARGS.pr)
+        drepo = gh.get_repo(f'{org_str}/{repo_str}')
+        dpr = drepo.get_pull(int(pr_str))
+        baserev = merge_base(target, dpr.base.sha, dpr.head.sha)
+        headrev = dpr.head.sha
+        dcommits = [c for c in dpr.get_commits()]
+        dshas = [c.sha for c in dcommits]
+        stdout(f'SHAs found in PR: {dshas}')
+    else:
+        baserev = ARGS.baserev
+        headrev = 'HEAD'
+
+    stdout(f'baserev: {baserev}')
+    stdout(f'headrev: {headrev}')
+    revs = runc_out(f'git -C {target} rev-list --first-parent {baserev}..{headrev}')
+    revs = revs.split('\n')
+    revs.reverse()
+    stdout(f'revs: {revs}')
+
+    rev_str = f"{','.join(revs)},"
+
+    for r in revs:
+        check_commit(urepo, target, r)
+
+    if dshas and dshas != revs:
+        sys.exit(f'{dshas} is different from {revs}')
 
 if __name__ == '__main__':
     main()
